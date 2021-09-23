2ccaa50181d8c2448ec8add30aa81774376602ea
Sphinx <quyuan2008@163.com>
2021-09-23 00:32:29 -0600

    changed the instruction

--- TitleScene.js
+++ TitleScene.js
@@ -11,22 +11,23 @@ Extends: Phaser.Scene,
 preload: function()
 {
     this.load.image('bg', 'assets/Pixel map draft 2.png');
-    this.load.image('instructions', 'assets/instructions.png');
+    this.load.image('instructions', 'assets/instructionpaper.png');
     this.add.text(10, 10, "Now Loading ...", { font: "20px Arial", fill: "#f8b229" });
     this.load.video('intro', 'assets/title.mp4');
+    this.load.audio('flip', 'assets/Sounds/flipover.ogg');
 },
 
 create:function()
 {
     this.add.image(256, 256, 'bg');
-    this.add.image(256, 256, 'instructions').setScale(0.8);
+    this.instruction = this.add.image(256, 256, 'instructions');
+    this.flip = this.sound.add("flip");
 
     this.video = this.add.video(256, 256, 'intro');
     this.video.play( );
     this.isPlaying = true;
 
     this.skipText = this.add.text(340, 10, "press space to skip", { fontSize: '13px', fill: '#f8b229' });
-
 
     this.video.on('complete', function(text = this.skipText){
         this.stop();
@@ -38,15 +39,19 @@ create:function()
 
 update:function(time)
 {
+    if (this.instruction.alpha < 1.0)
+        this.instruction.alpha += 0.02;
     if (this.cursors.space.isDown)
     {
         if ( this.isPlaying )
         {
             this.video.stop();
             this.video.destroy();
-            this.skipText.setText("press space to START!");
+            this.skipText.destroy( );
             this.isPlaying = false;
             this.pressTime = time;
+            this.instruction.alpha = 0.1;
+            this.flip.play();
         }
         else if( time > this.pressTime + 1000 )
         {
